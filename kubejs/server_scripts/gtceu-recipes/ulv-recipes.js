74454597234066b1db796cc7e73f549ebd00d765
HazeVista <zetateamhazevista@gmail.com>
2025-10-24 06:49:08 +0000

    0.0.1 10-24-25 Haze farmer's delight changes

--- kubejs/server_scripts/gtceu-recipes/ulv-recipes.js
+++ kubejs/server_scripts/gtceu-recipes/ulv-recipes.js
@@ -1,62 +1,38 @@
 ServerEvents.recipes(event => {
 
-    event.shaped('gtceu:rubber_plate', [
-        'A',
-        'B',
-        'B'
-    ] , {
-        A: '#forge:tools/hammers',
-        B: 'gtceu:rubber_ingot'
-    })
+  event.shaped('gtceu:rubber_plate', [
+    'A',
+    'B',
+    'B'
+  ] , {
+    A: '#forge:tools/hammers',
+    B: 'gtceu:rubber_ingot'
+  })
 
-    event.recipes.gtceu.alloy_smelter('alloy_smelter_rose_quartz')
+  event.recipes.gtceu.alloy_smelter('alloy_smelter_rose_quartz')
     .itemInputs('minecraft:quartz', '4x minecraft:redstone')
     .itemOutputs('create:rose_quartz')
     .duration(50)
     .EUt(30)
 
-    event.campfireCooking('gtceu:glass_dust', 'minecraft:glass', 0, 160)
-    event.campfireCooking('minecraft:iron', 'gtceu:wrought_iron', 0, 160)
+  event.campfireCooking('gtceu:glass_dust', 'minecraft:glass', 0, 160)
+  event.campfireCooking('minecraft:iron_ingot', 'gtceu:wrought_iron_ingot', 0, 160)
 
-    event.custom({
-        'type': 'farmersdelight:cooking',
-        'recipe_book_tab': 'misc',
-        'ingredients': [
-          {
-            'item': 'gtceu:raw_rubber_pulp',
-            'count': 3,
-          },
-          {
-            'item': 'gtceu:sulfur_dust'
-          }
-        ],
-        'result': {
-          'item': 'gtceu:rubber_ingot',
-          'count': 1,
-        },
-        'cookingtime': 320
-    });
+  event.recipes.farmersdelight.cooking(
+    ['3x gtceu:raw_rubber_pulp', 'gtceu:sulfur_dust'],
+    'gtceu:rubber_ingot',
+    0,
+    320,
+    'gtceu:brick_wooden_form'
+    );
 
-    event.custom({
-        'type': 'farmersdelight:cooking',
-        'recipe_book_tab': 'misc',
-        'ingredients': [
-          {
-            'item': 'gtceu:desh_dust'
-          },
-          {
-            'item': 'gtceu:iron_dust'
-          },
-          {
-            'item': 'gtceu:red_alloy_dust'
-          }
-        ],
-        'result': {
-          'item': 'powah:energized_steel',
-          'count': 3,
-        },
-        'cookingtime': 200
-    });
+  event.recipes.farmersdelight.cooking(
+    ['gtceu:desh_dust', 'gtceu:iron_dust', 'gtceu:red_alloy_dust'],
+    '3x powah:energized_steel',
+    0,
+    200,
+    'gtceu:brick_wooden_form'
+    )
 
     event.recipes.gtceu.mixer('mixing_energized_steel_dust')
         .itemInputs('gtceu:desh_dust', 'gtceu:iron_dust', 'gtceu:red_alloy_dust')
