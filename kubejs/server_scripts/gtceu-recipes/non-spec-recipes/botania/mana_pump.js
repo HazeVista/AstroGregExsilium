cda2ce3aaeb51cfbca77df2bb7c4aa67e9b1f31c
digestlotion <digestlotion@proton.me>
2025-12-22 22:42:17 +0800

    0.0.1 22-12-25 manafield simulator

--- kubejs/server_scripts/gtceu-recipes/non-spec-recipes/botania/mana_pump.js
+++ kubejs/server_scripts/gtceu-recipes/non-spec-recipes/botania/mana_pump.js
@@ -1,45 +1,69 @@
 ServerEvents.recipes(event => {
-const fabricator = event.recipes.gtceu.manafield_simulator
 
-// #region mana production
+    // #region MANA SIMULATION
+    const fabricator = (id) => event.recipes.gtceu.manafield_simulation(id);
+    const mana = 1;
 
+    [
+        ['minecraft:water 1000', 'botania:hydroangeas', 500],
+        ['minecraft:lava 1000', 'botania:thermalily', 10000]
+    ].forEach(([i, f, c]) => {
+        let flowerName = f.split(':')[1];
+        fabricator(`${flowerName}_simulation`)
+            .inputFluids(i)
+            .chancedInput(f, c, 0)
+            .outputFluids(`manafluid:mana ${mana}`)
+            .duration(mana * 200)
+            .EUt(28);
+    });
 
+    [
+        ['#forge:fuels', 'botania:endoflame'],
+        ['64x #minecraft:leaves', 'botania:munchdew'],
+        ['#forge:tnt', 'botania:entropinnyum'],
+        ['minecraft:cake', 'botania:kekimurus'],
+        ['#c:foods', 'botania:gourmaryllis']
+    ].forEach(([i, f]) => {
+        let flowerName = f.split(':')[1];
+        fabricator(`${flowerName}_simulation`)
+            .itemInputs(i, f)
+            .outputFluids(`manafluid:mana ${mana}`)
+            .duration(mana * 200)
+            .EUt(28);
+    });
+    // #endregion
 
-// #endregion
+    // #region shaped singleblocks
+    const tieredParts = [
+        ['lv', 'tin', 'botania:managlass'],
+        ['mv', 'copper', 'botania:managlass'],
+        ['hv', 'gold', 'botania:managlass'],
+        ['ev', 'aluminium', 'botania:alfglass'],
+        ['iv', 'platinum', 'botania:alfglass'],
+        ['luv', 'niobium_titanium', 'botania:alfglass'],
+        ['zpm', 'vanadium_gallium', 'botania:bifrost_perm'],
+        ['uv', 'yttrium_barium_cuprate', 'botania:bifrost_perm']
+    ]
 
+    tieredParts.forEach(singleblocks)
 
+    function singleblocks(part) {
+        const tier = part[0]
+        const cable = part[1]
+        const glass = part[2]
 
-// #region shaped singleblocks
-const tieredParts = [
-['lv', 'tin', 'botania:managlass'],
-['mv', 'copper', 'botania:managlass'],
-['hv', 'gold', 'botania:managlass'],
-['ev', 'aluminium', 'botania:alfglass'],
-['iv', 'platinum', 'botania:alfglass'],
-['luv', 'niobium_titanium', 'botania:alfglass'],
-['zpm', 'vanadium_gallium', 'botania:bifrost_perm'],
-['uv', 'yttrium_barium_cuprate', 'botania:bifrost_perm']
-]
-
-tieredParts.forEach(singleblocks)
-
-function singleblocks(part) {
-    const tier = part[0]
-    const cable = part[1] 
-    const glass = part[2] 
-
-    event.shaped(`gtceu:${tier}_manafield_simulator`, [
-        'DAD',
-        'FEF',
-        'BCB'
-    ], {
-        A: `gtceu:${tier}_emitter`,
-        B: `gtceu:${tier}_electric_pump`,
-        C: `gtceu:${cable}_single_cable`,
-        D:  glass,
-        E: `gtceu:${tier}_machine_hull`,
-        F: `#gtceu:circuits/${tier}`
-    })
-}
-// #endregion
+        event.shaped(`gtceu:${tier}_manafield_simulator`, [
+            'DAD',
+            'FEF',
+            'BCB'
+        ], {
+            A: `gtceu:${tier}_emitter`,
+            B: `gtceu:${tier}_electric_pump`,
+            C: `gtceu:${cable}_single_cable`,
+            D: glass,
+            E: `gtceu:${tier}_machine_hull`,
+            F: `#gtceu:circuits/${tier}`
+        })
+    }
+    // #endregion
 })
