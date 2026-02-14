026ad192db653e94e4e529a436fe5af630d32939
HazeVista <zetateamhazevista@gmail.com>
2026-02-14 18:47:18 +0000

    0.0.1 Haze broken sonny model for now

---
+++ kubejs/startup_scripts/blocks/misc_blocks/plush.js
@@ -0,0 +1,12 @@
+StartupEvents.registry('block', event => {
+
+    // event.create('astrogreg:sonny', 'cardinal')
+    //     .translationKey('block.astrogreg.sonny')
+    //     .hardness(0.2)
+    //     .resistance(10)
+    //     .soundType('wool')
+    //     .mapColor('terracotta_yellow')
+    //     .renderType('cutout')
+    //     .defaultCutout()
+
+})
